90d66d4f48ad1bb4e0759f9d8675e2b7ca18c0be
yyyXXX <yyyXXX@outlook.com>
2025-11-07 10:57:53 +0800

    test_func_symbol 测试代码132

--- symbol_table/symbol_table_testcode.cpp
+++ symbol_table/symbol_table_testcode.cpp
@@ -1,10 +1,19 @@
 #include "symbol.h"
+#include <iostream>
 
 int main() {
     std::string var_name = "a";
-    Symbol* var = new Symbol(var_name, SymbolType:: var, 4);
-    SymbolTable* symbol_table;
-    symbol_table->setOffset(symbol_table->getOffset()+var->getWidth());
+    Symbol* var = new Symbol(var_name, SymbolType::var, 4);
+
+    // 修复：实例化 SymbolTable 对象
+    SymbolTable* symbol_table = new SymbolTable(false);  // 或者使用栈对象
+
+    symbol_table->setOffset(symbol_table->getOffset() + var->getWidth());
     var->setSymOffset(symbol_table->getOffset());
     symbol_table->addSymbol(var);
+
+    // 清理资源
+    delete symbol_table;
+
+    return 0;
 }
