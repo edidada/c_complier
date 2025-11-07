856d7863b10334de40824c97a0fa53ff7073a7ac
yyyXXX <yyyXXX@outlook.com>
2025-11-07 10:22:08 +0800

    test_func_symbol impl

--- abstract_syntax_tree/DefFunASTNode.cpp
+++ abstract_syntax_tree/DefFunASTNode.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "DefFunASTNode.h"
+#include <iostream>
+
+// DefFunASTNode 实现
+DefFunASTNode::DefFunASTNode() {
+    // 初始化代码
+}
+
+DefFunASTNode::DefFunASTNode(AstNodeType nodeType, char* content)
+    : AbstractAstNode(nodeType, content) {
+    // 初始化代码
+}
+
+DefFunASTNode::~DefFunASTNode() {
+    // 清理资源
+}
+
+AbstractAstNode* DefFunASTNode::getArgList() {
+}
+
+// DefVarASTNode 实现
+DefVarASTNode::DefVarASTNode() {
+}
+
